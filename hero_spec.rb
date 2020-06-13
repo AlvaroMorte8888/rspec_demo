faf1389117459dba6dffd31286c32c5358235426
AlvaroMorte8888 <alvaromorte8888@gmail.com>
2020-06-13 12:10:31 +0300

    Add test for power_up

--- hero_spec.rb
+++ hero_spec.rb
@@ -7,4 +7,10 @@ describe Hero do
         expect(hero.name).to eq 'Mike' # аналог hero.name == 'Mike
         #expect - ожидаем
     end    
-end            
+
+    it "can power_up" do 
+        hero = Hero.new 'mike'
+
+        expect(hero.power_up).to eq 110
+    end    
+end  
